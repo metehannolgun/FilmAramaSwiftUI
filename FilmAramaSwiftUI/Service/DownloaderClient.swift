2a728e18687705681a33909d7eb595cfe5ced541
metehannolgun <metehannolgun@gmail.com>
2025-06-05 16:41:51 +0300

    -Film arama SearchBar 'ı yapıldı. -Detay modeli yazıldı ve download client güncellendi

--- FilmAramaSwiftUI/Service/DownloaderClient.swift
+++ FilmAramaSwiftUI/Service/DownloaderClient.swift
@@ -26,6 +26,29 @@ class DownloaderClient {
             completion(.success(filmCevabi.filmler))
         }.resume()
     }
+    
+    func filmDetayiniIndir(imdbId: String, completion: @escaping (Result<FilmDetay,DownloaderError>) -> Void) {
+        // Detay indirme işlemleri burada yapılacak.
+        guard let url = URL(string: "https://www.omdbapi.com/?i=\(imdbId)&apikey=4a0cf678") else {
+            return completion(.failure(.yanlisUrl))
+    }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return completion(.failure(.veriGelmedi))
+            }
+            
+            guard let gelenFilmDetayi = try? JSONDecoder().decode(FilmDetay.self, from: data) else {
+                return completion(.failure(.veriIslenemedi))
+            }
+            
+            completion(.success(gelenFilmDetayi))
+            
+        }.resume()
+        
+        }
+    
+    
+    
 }
 
 enum DownloaderError: Error {
--- FilmAramaSwiftUI/View/FilmListeView.swift
+++ FilmAramaSwiftUI/View/FilmListeView.swift
@@ -10,33 +10,44 @@ import SwiftUI
 struct FilmListeView: View {
     
     @ObservedObject var filmListeViewModel : FilmListeViewModel
+    @State var aranacakFilm = ""
+    
     init() {
         self.filmListeViewModel = FilmListeViewModel()
-        self.filmListeViewModel.filmAramasiYap(filmIsmi: "wonder")
+       
     }
         var body: some View {
             NavigationView {
-                List(filmListeViewModel.filmler, id: \.imdbId) { film in
-                    HStack {
-                        OzelImage(url: film.poster)
-                            .frame(width: 90,height: 130)
-                        
-                        VStack(alignment: .leading) {
-                            Text(film.title)
-                                .font(.title3)
-                                .foregroundColor(.blue)
-                            
-                            Text(film.year)
-                                .foregroundColor(.orange)
-                            
-                        }
-                    }
+                
+                VStack {
+                    TextField("Aranacak Film", text: $aranacakFilm, onEditingChanged: { _ in }, onCommit: {
+                        self.filmListeViewModel.filmAramasiYap(filmIsmi: aranacakFilm.trimmingCharacters(in: .whitespacesAndNewlines).addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? aranacakFilm)
+                    }).padding()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .font(.title2)
                     
-                }.navigationTitle(Text("Film Arama"))
+                    
+                    List(filmListeViewModel.filmler, id: \.imdbId) { film in
+                        HStack {
+                            OzelImage(url: film.poster)
+                                .frame(width: 90,height: 130)
+                            
+                            VStack(alignment: .leading) {
+                                Text(film.title)
+                                    .font(.title3)
+                                    .foregroundColor(.blue)
+                                
+                                Text(film.year)
+                                    .foregroundColor(.orange)
+                                
+                            }
+                        }
+                        
+                    }.navigationTitle(Text("Film Arama"))
+                    
+                }
                 
             }
-            
-        
     }
         
 }
